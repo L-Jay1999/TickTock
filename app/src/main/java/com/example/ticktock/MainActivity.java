3d9fc7bac868e88cf4987be7f116a401b11e1321
Jay <406336801@qq.com>
2020-05-25 11:47:56 +0800

    add retrofit

--- app/src/main/java/com/example/ticktock/MainActivity.java
+++ app/src/main/java/com/example/ticktock/MainActivity.java
@@ -3,6 +3,18 @@ package com.example.ticktock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.ticktock.network.ApiService;
+import com.example.ticktock.network.VideoResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getData();
+    }
+
+    public void getData() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://beiyou.bytedance.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+        apiService.getVideoInfos().enqueue(new Callback<List<VideoResponse>>() {
+            @Override
+            public void onResponse(Call<List<VideoResponse>> call, Response<List<VideoResponse>> response) {
+                if(response.body() != null) {
+                    List<VideoResponse> videoInfoList = response.body();
+                    Log.d("retrofit", videoInfoList.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<VideoResponse>> call, Throwable t) {
+                Log.d("retrofit", t.getMessage());
+            }
+        });
     }
 }
